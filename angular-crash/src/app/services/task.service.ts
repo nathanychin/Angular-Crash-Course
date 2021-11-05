1a3fa378ccdd3a56bc75838c19e2361e95446077
Nathan Y. C <50110608+nathanychin@users.noreply.github.com>
2021-11-05 15:12:07 -0400

    Angular HTTP set up
    Set up Angul;ar HTTP Client

--- angular-crash/src/app/services/task.service.ts
+++ angular-crash/src/app/services/task.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs'
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs' // removed 'of' from imports
 import {Task} from '../Task';
-import {TASKS} from '../mock-tasks';
+// import {TASKS} from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
     getTasks(): Observable<Task[]> {
-      const tasks = of(TASKS); // of() turns object into Observable type for getTasks() to return
-
-      return tasks;
+      //const tasks = of(TASKS); // of() turns object into Observable type for getTasks() to return
+     // return tasks;
+     return this.http.get<Task[]>(this.apiUrl)
     }
 
 }
